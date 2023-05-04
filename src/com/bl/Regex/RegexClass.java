34f66ed21c50b6b0fd19a0f8af8e7fc69b4f6f2d
aastha <aasthabarve0@gmail.com>
2023-05-04 18:12:27 +0530

    UC7 Password validation 3

--- src/com/bl/Regex/RegexClass.java
+++ src/com/bl/Regex/RegexClass.java
@@ -2,11 +2,11 @@ package com.bl.Regex;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class RegexClass {
-    static Pattern pattern = Pattern.compile("^(?=[1-9a-zA-Z])(?=.*[A-Z]).{8}");
-// at least 1 upper case ("^(?=[1-9a-zA-Z])(?=.*[A-Z]).{8}");
+    static Pattern pattern = Pattern.compile("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$");
+// ("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$");
 
     public static void main(String[] args) {
-        String str = "asthS";
+        String str = "asthSba3";
         Matcher m = pattern.matcher(str);
         if (m.matches()) {
             System.out.println("valid Password");
