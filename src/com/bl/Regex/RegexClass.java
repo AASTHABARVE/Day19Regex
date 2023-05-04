30c2f6c8b3660547d38b9772722092cca182af8f
AASTHABARVE <84262688+AASTHABARVE@users.noreply.github.com>
2023-05-04 18:27:34 +0530

    Update RegexClass.java

--- src/com/bl/Regex/RegexClass.java
+++ src/com/bl/Regex/RegexClass.java
@@ -3,7 +3,6 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class RegexClass {
     static Pattern pattern = Pattern.compile("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[%^_#&$+=@]).{8,}$");
-// at least 1 special character
 
     public static void main(String[] args) {
         String str = "asthSba3@";
