64d993835d5ba2bed94a53f20015eb59b6b5eaa9
aastha <aasthabarve0@gmail.com>
2023-05-04 17:24:03 +0530

    UC5 Password Validation

--- src/com/bl/Regex/RegexClass.java
+++ src/com/bl/Regex/RegexClass.java
@@ -2,16 +2,16 @@ package com.bl.Regex;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class RegexClass {
-    static Pattern pattern = Pattern.compile("^(0|91)\\s?[7-9][0-9]{9}$");
+    static Pattern pattern = Pattern.compile("^[a-zA-Z][a-zA-Z]{8}$");
 
     public static void main(String[] args) {
-        String str = "91 9919819801";
+        String str = "AasthaxyZ";
         Matcher m = pattern.matcher(str);
         if (m.matches()) {
-            System.out.println("valid Number");
+            System.out.println("valid Password");
         }
         else {
-            System.out.println("invalid Number");
+            System.out.println("invalid Password");
         }
     }
 }
