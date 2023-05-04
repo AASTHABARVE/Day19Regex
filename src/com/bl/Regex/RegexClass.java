030260b31518e00080362367b5ddd2bae5e35b42
aastha <aasthabarve0@gmail.com>
2023-05-04 16:59:32 +0530

    UC3 Email Validation

--- src/com/bl/Regex/RegexClass.java
+++ src/com/bl/Regex/RegexClass.java
@@ -2,16 +2,16 @@ package com.bl.Regex;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class RegexClass {
-    static Pattern pattern = Pattern.compile("^[A-Z a-z]{3,}$");
+    static Pattern pattern = Pattern.compile("^[a-zA-Z+_.-]+\\.[A-Za-z+_.-]+@(?:[a-z])+(?:\\.[a-zA-Z]{2})+(?:\\.[a-zA-Z)]{2})$");
 
     public static void main(String[] args) {
-        String str = "Reg";
+        String str = "abc.xyz@bl.co.in";
         Matcher m = pattern.matcher(str);
         if (m.matches()) {
-            System.out.println("valid last name");
+            System.out.println("valid email");
         }
         else {
-            System.out.println("invalid last name");
+            System.out.println("invalid email");
         }
     }
 }
