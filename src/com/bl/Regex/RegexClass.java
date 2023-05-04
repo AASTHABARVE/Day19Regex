e86ab023947254ee401961221a66e902a4f33d6a
aastha <aasthabarve0@gmail.com>
2023-05-04 18:18:52 +0530

    UC8 password validation 4

--- src/com/bl/Regex/RegexClass.java
+++ src/com/bl/Regex/RegexClass.java
@@ -2,11 +2,11 @@ package com.bl.Regex;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class RegexClass {
-    static Pattern pattern = Pattern.compile("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$");
-// ("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$");
+    static Pattern pattern = Pattern.compile("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[%^_#&$+=@]).{8,}$");
+// at least 1 special character
 
     public static void main(String[] args) {
-        String str = "asthSba3";
+        String str = "asthSba3@";
         Matcher m = pattern.matcher(str);
         if (m.matches()) {
             System.out.println("valid Password");
