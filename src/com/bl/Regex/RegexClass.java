6e0be5ac5d191e5fa034df420b33432c57c24c7b
aastha <aasthabarve0@gmail.com>
2023-05-04 17:16:48 +0530

    UC4 Mobile Number Validation

--- src/com/bl/Regex/RegexClass.java
+++ src/com/bl/Regex/RegexClass.java
@@ -2,16 +2,16 @@ package com.bl.Regex;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class RegexClass {
-    static Pattern pattern = Pattern.compile("^[a-zA-Z+_.-]+\\.[A-Za-z+_.-]+@(?:[a-z])+(?:\\.[a-zA-Z]{2})+(?:\\.[a-zA-Z)]{2})$");
+    static Pattern pattern = Pattern.compile("^(0|91)\\s?[7-9][0-9]{9}$");
 
     public static void main(String[] args) {
-        String str = "abc.xyz@bl.co.in";
+        String str = "91 9919819801";
         Matcher m = pattern.matcher(str);
         if (m.matches()) {
-            System.out.println("valid email");
+            System.out.println("valid Number");
         }
         else {
-            System.out.println("invalid email");
+            System.out.println("invalid Number");
         }
     }
 }
